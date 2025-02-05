cea0c25053fa49fd1f5b7d2290bc867c7ca2365d
valow <80098893+valow-psd@users.noreply.github.com>
2025-02-05 08:54:19 +0100

    init plateau

--- TP5/TP5/Plateau.java
+++ TP5/TP5/Plateau.java
@@ -5,8 +5,22 @@ class Plateau {
     private int[][] plateauInitial;
     private int[][] plateauActuel;
     private int[][] plateauFinal;
-
     private List<Integer> seqMin = new ArrayList<>();
     private int x0, y0, size;
+
+    public Plateau(int[][] plateauInitial, int[][] plateauFinal) {
+        this.plateauInitial = plateauInitial;
+        this.plateauActuel = plateauInitial;
+        this.plateauFinal = plateauFinal;
+        this.size = plateauInitial.length;
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                if (plateauInitial[i][j] == 0) {
+                    x0 = i;
+                    y0 = j;
+                }
+            }
+        }
+    }
 }
 
---
+++ TP5/TP5/TaquinSolveur.java
@@ -0,0 +1,7 @@
+import java.util.ArrayList;
+import java.util.List;
+
+// Solveur du jeu du Taquin
+class TaquinSolveur {
+  
+}
